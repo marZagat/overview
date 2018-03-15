0793a428d9c068bdac034b3b350ae44a2e3490fa
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-15 11:33:44 -0700

    use environment variables for seed.js settings

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const faker = require('faker');
 const { MongoClient } = require('mongodb');
 
 const dbAddress = process.env.DB_ADDRESS || 'localhost';
-const url = `mongodb://${dbAddress}:27017`;
-const dbName = 'marzagat_overview';
+const dbName = process.env.DB_NAME || 'marzagat_overview';
+const collectionName = process.env.COLLECTION_NAME || 'restaurants';
+const seedNum = parseInt(process.env.SEED_NUM, 10) || 10000000;
+const batchSize = parseInt(process.env.SEED_BATCH_SIZE, 10) || 5000;
 
-const settings = {
-  numToAdd: 10000000,
-  batchSize: 10000,
-};
+const url = `mongodb://${dbAddress}:27017`;
 
 const generateDocument = id => (
   {
@@ -44,8 +44,8 @@ const seedBatch = (minId, maxId, collection) => (
 
 const seedDb = async (num, collection) => {
   try {
-    for (let i = 0; i < num; i += settings.batchSize) {
-      await seedBatch(i, i + settings.batchSize, collection);
+    for (let i = 0; i < num; i += batchSize) {
+      await seedBatch(i, i + batchSize, collection);
     }
     console.log('done seeding db!');
   } catch (error) {
@@ -55,8 +55,8 @@ const seedDb = async (num, collection) => {
 
 MongoClient.connect(url)
   .then(async (client) => {
-    const collection = client.db(dbName).collection('restaurants');
-    await seedDb(settings.numToAdd, collection);
+    const collection = client.db(dbName).collection(collectionName);
+    await seedDb(seedNum, collection);
     client.close();
   })
   .catch((error) => {

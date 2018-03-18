cf62a2e4d31d383f42b658b14e018774c21158e6
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-18 15:49:44 -0700

    new structure for benchmarking using .explain()

--- db/dbBenchmarkTests.js
+++ db/dbBenchmarkTests.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const Promise = require('bluebird');
+const stats = require('stats-lite');
 
 const dbAddress = process.env.DB_ADDRESS || 'localhost';
 const dbName = process.env.DB_NAME || 'marzagat_overview';
 const collectionName = process.env.COLLECTION_NAME || 'restaurants';
 const dbSize = parseInt(process.env.DB_SIZE, 10) || 10000000;
 const index = process.env.INDEX || '_id';
+const idType = process.env.ID_TYPE || 'number';
 
 const connectToDb = async () => {
   const url = `mongodb://${dbAddress}/`;
@@ -15,82 +17,122 @@ const connectToDb = async () => {
   return { client, collection };
 };
 
-const timer = async (testDescription, callback) => {
-  const startTime = new Date().getTime();
-  await callback();
-  const endTime = new Date().getTime();
-  const executionTime = endTime - startTime;
-  console.log(`${testDescription}: executed in ${executionTime} ms`);
-};
+// const timer = async (testDescription, callback) => {
+//   const startTime = new Date().getTime();
+//   await callback();
+//   const endTime = new Date().getTime();
+//   const executionTime = endTime - startTime;
+//   console.log(`${testDescription}: executed in ${executionTime} ms`);
+// };
 
-const getRandomId = () => Math.floor(Math.random() * dbSize);
-
+const getId = idNum => (
+  idType === 'string'
+    ? idNum.toString()
+    : idNum
+);
+const getRandomId = () => getId(Math.floor(Math.random() * dbSize));
 const getOptions = (id) => {
   const options = {};
-  if (index !== '_id') {
-    options[index] = id.toString();
-  } else {
-    options[index] = id;
-  }
-
+  options[index] = id;
   return options;
 };
 
-const runTests = async (name1, name2) => {
-  console.log(`\n\nTESTING SCHEMA WITH INDEX: ${index}`);
+const getExecutionStats = (queryFunction) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { executionStats } = await queryFunction().explain();
+      if (executionStats.executionSuccess === true) {
+        resolve(executionStats);
+      } else {
+        reject(new Error('query execusionSuccess !== true'));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+const getQueryTime = (queryFunction) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { executionTimeMillis } = await getExecutionStats(queryFunction);
+      resolve(executionTimeMillis);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const timer = async (message, queryFunction) => {
+  try {
+    const { executionTimeMillis } = await getExecutionStats(queryFunction);
+    console.log(`${message}:\n  => success: ${executionTimeMillis} ms`);
+  } catch (error) {
+    console.warn(`${message}: UNSUCCESSFUL QUERY`, error);
+  }
+};
+
+const summaryStats = array => (
+  {
+    min: Math.min(...array),
+    median: stats.median(array),
+    p95: stats.percentile(array, 0.95),
+    p99: stats.percentile(array, 0.99),
+    p995: stats.percentile(array, 0.995),
+    max: Math.max(...array),
+  }
+);
+
+const test = testFunction => testFunction();
+
+const runTests = async () => {
+  console.log(`\n\nTESTING SCHEMA WITH INDEX: ${index}, ID TYPE: ${idType}`);
   console.log('===================================\n');
 
   const { client, collection } = await connectToDb();
-  let testId;
 
-  testId = getRandomId();
-  for (let i = 0; i < 10; i++) {
-    await timer(`sequential individual query #${i}`, () => (
-      collection.find(getOptions(testId))
-    ));
-  }
+  // 5 sequential queries on the same item
+  await test(async () => {
+    const testId = getRandomId();
+    const options = getOptions(testId);
+    for (let i = 0; i < 5; i++) {
+      await timer(`query #${i} on same id`, () => collection.find(options));
+    }
+  });
 
-  await timer(`1000 async queries on a single item`, async () => {
-    const promises = [];
-    testId = getRandomId();
+  // 1,000 queries on the same id
+  await test(async () => {
+    const queryTimePromises = [];
+    const testId = getRandomId();
+    const options = getOptions(testId);
     for (let i = 0; i < 1000; i++) {
-      promises.push(collection.find(getOptions(testId)));
+      queryTimePromises.push(getQueryTime(() => collection.find(options)));
     }
-    return Promise.all(promises);
+    const queryTimes = await Promise.all(queryTimePromises);
+    const { min, median, p95, p99, p995, max } = summaryStats(queryTimes);
+
+    console.log('1,000 queries on same id: distribution (ms)');
+    console.log(`  => min: ${min} | median: ${median} | p95: ${p95} | p99: ${p99} | p995: ${p995} | max: ${max}`);
   });
 
-  await timer(`10000 async queries on a single item`, async () => {
-    const promises = [];
-    testId = getRandomId();
-    for (let i = 0; i < 10000; i++) {
-      promises.push(collection.find(getOptions(testId)));
-    }
-    return Promise.all(promises);
+  // 10,000 queries on the same id
+  await test(async () => {
   });
 
-  let thousandIds = new Array(1000).map(() => getRandomId());
-  await timer(`1000 async queries on different items`, async () => {
-    const promises = [];
-    thousandIds.forEach((id) => {
-      promises.push(collection.find(getOptions(id)));
-    });
-    return Promise.all(promises);
+  // 1,000 queries on different ids
+  await test(async () => {
+
   });
 
-  const tenThousandIds = new Array(10000).map(() => getRandomId());
-  await timer(`10000 async queries on different items`, async () => {
-    const promises = [];
-    tenThousandIds.forEach((id) => {
-      promises.push(collection.find(getOptions(id)));
-    });
-    return Promise.all(promises);
+  // 10,000 queries on different ids
+  await test(async () => {
+
   });
 
-  for (let i = 0; i < 10; i++) {
-    await timer(`sequential query #${i} for a batch of 1000 documents`, () => (
-      collection.find({}).limit(1000).toArray()
-    ));
-  }
+  // sequential queries on a batch of 1,1000 documents
+  await test(async () => {
+
+  });
 
   client.close();
 };

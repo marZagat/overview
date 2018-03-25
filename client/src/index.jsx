3b1929771be8441ac9df34c940ecd162bb1b78ba
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-25 14:40:41 -0700

    render react component from html

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Overview from './components/Overview';
 import './style.css';
 
-ReactDOM.render(<Overview />, document.getElementById('overview-app'));
+window.Overview = Overview;
